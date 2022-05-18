7426588a5cf04accad9821643f1233466a41a19f
Ivan Lotarev <lotarevivan@gmail.com>
2022-05-18 18:11:38 +0300

    task1 работает

--- task1.js
+++ task1.js
@@ -13,74 +13,72 @@ a.b.c
 
 
 
-// const o = {
-//     one:'1',
-//     two:'2',
-//     three:{
-//         four:'4'
-//     },
-//     five:{
-//         six:'6'
-//     }
-// }
-
-// const key_array=['three.four','five.six']
-// function copy(obj,arr) {
-//     let resultArr=[]
-//     // if(!obj||!arr){
-//     //     return 'no arguments'
-//     // }
-    
-//     arr.forEach( element => {
-//         let temp = obj
-//         let deep = element.split('.')
-//         for (let i=0;i<deep.length;i++){
-//             if(temp[deep[i]]){
-//                 temp=temp[deep[i]]
-//             }else{
-//                 return undefined
-//             }
-//             // console.log(deep[i])
-//             // console.log(obj[deep[i]])
-//         }
-    
-//         resultArr.push([element,temp])
-//     });
-
-//     return  resultArr
-//     // return  Object.fromEntries(resultArr)
-// }
-// copy(o,key_array)
-// console.log('original:',o)
-// console.log('original:',o.three.four)
-// console.log('copied:',copy(o,key_array))
-
-
-
-const arr = [ [ 'three.four', '4' ], [ 'five.six', '6' ] ]
-
-
-const res = arr.forEach(el=>{
-    let deep = el[0].split('.')
-    let val=el[1]
-    // console.log(deep)
-    let temp={}
-    for(let i=deep.length-1;i>=0;i--){
-        // console.log('d',deep[i])
-        // console.log('t.d',(temp[deep[i]]))
-        // console.log('t')
-        if(!temp[deep[i+1]]){
-            temp[deep[i]]=val
-        }else{
-            temp[deep[i]]= JSON.parse(JSON.stringify(temp))
-            delete temp[deep[i+1]]
-            console.log(temp)
-        }
-        // console.log('-----------------');
+const o = {
+    one:'1',
+    two:'2',
+    three:{
+        four:'4'
+    },
+    five:{
+        six:'6'
     }
-    // console.log(temp)
-    // return temp
-})
+}
 
+const key_array=['three.four','five.six']
+function copy(obj,arr) {
+    let resultArr=[]
+    // if(!obj||!arr){
+    //     return 'no arguments'
+    // }
+    
+    arr.forEach( element => {
+        let temp = obj
+        let deep = element.split('.')
+        for (let i=0;i<deep.length;i++){
+            if(temp[deep[i]]){
+                temp=temp[deep[i]]
+            }else{
+                return undefined
+            }
+            // console.log(deep[i])
+            // console.log(obj[deep[i]])
+        }
+    
+        resultArr.push([element,temp])
+    });
+
+    return  resultArr.map(el=>{
+        let deep = el[0].split('.')
+        let val=el[1]
+        // console.log(deep)
+        let temp={}
+        for(let i=deep.length-1;i>=0;i--){
+            // console.log('d',deep[i])
+            // console.log('t.d',(temp[deep[i]]))
+            // console.log('t')
+            if(!temp[deep[i+1]]){
+                temp[deep[i]]=val
+            }else{
+                temp[deep[i]]= JSON.parse(JSON.stringify(temp))
+                delete temp[deep[i+1]]
+                // console.log(temp)
+            }
+            // console.log('-----------------');
+        }
+        console.log(temp)
+        return temp
+    })[0]
+    // return  Object.fromEntries(resultArr)
+}
+// copy(o,key_array)
+let copied = copy(o,key_array)
+console.log('test modify copied .three.four')
+copied.three.four='test'
+console.log('original .three.four:',o.three.four)
+console.log('copied .three.four :',copied.three.four)
+// console.log('original:',o)
+// console.log('copied:',copy(o,key_array))
+// const arr = [ [ 'three.four', '4' ], [ 'five.six', '6' ] ]
+// const res = arr.
 // console.log(res)
 
